41b8776001ddb0945da034230140a38ad9154356
Landon Dyken <ldyken53@uab.edu>
2022-04-11 15:26:40 -0500

    Add score, runtime, and votes

--- scraper/imdb.py
+++ scraper/imdb.py
@@ -1,5 +1,6 @@
 
 import asyncio
+import csv
 from urllib.parse import urljoin
 
 import requests
@@ -35,9 +36,21 @@ async def traverse_imdb_movies(url):
     current_url = url
     while True:
         raw_html = await get_url_content(current_url)
+        score, votes, runtime = ("MISSING",) * 3
         context = html.fromstring(raw_html)
-        movies = sxpath(context, ".//body//h3[@class='lister-item-header']/a")
+        # movies = sxpath(context, ".//body//h3[@class='lister-item-header']/a")
+        movies = sxpath(context, ".//body//div[@class='lister-item-content']")
         for movie_node in movies:
+            score_html = sxpath(movie_node, ".//div/div/strong")
+            runtime_html = sxpath(movie_node, ".//span[@class='runtime']")
+            votes_html = sxpath(movie_node, ".//span[@name='nv']")
+            if score_html:
+                score = score_html[0].text_content()
+            if runtime_html:
+                runtime = runtime_html[0].text_content()
+            if votes_html:
+                votes = votes_html[0].get("data-value")
+            movie_node = sxpath(movie_node, ".//h3[@class='lister-item-header']/a")[0]
             movie_title = movie_node.text_content()
             partial_movie_url = movie_node.get("href")
             full_movie_url = urljoin(url, partial_movie_url)
@@ -47,7 +60,9 @@ async def traverse_imdb_movies(url):
             except Exception as e:
                 print(f"Encountered error for {movie_title}: {str(e)}")
                 reviews = {}
-                
+            reviews["imdb_score"] = score
+            reviews["imdb_votes"] = votes
+            reviews["runtime"] = runtime
             movie_review = { "title": movie_title, "reviews": reviews }
             all_reviews.append(movie_review)
         partial_next_page_url = sxpath(context, ".//body//a[@class='lister-page-next next-page']/@href")
@@ -90,7 +105,7 @@ async def get_movie_reviews(url: str) -> dict:
 
 
 def json_to_csv(data: dict):
-    expected_fields = ["user_review_count", "critic_review_count", "critic_overall_score", "critic_positive_score", 
+    expected_fields = ["runtime", "imdb_score", "imdb_votes", "user_review_count", "critic_review_count", "critic_overall_score", "critic_positive_score", 
     "critic_mixed_score", "critic_negative_score", "user_overall_score", "user_positive_score", "user_mixed_score", 
     "user_negative_score"]  # this expects the names to be consistent upon creation
 
@@ -109,7 +124,7 @@ def json_to_csv(data: dict):
 
 
 def main():
-    base_url = "https://www.imdb.com/search/title/?groups=oscar_winner&sort=release_date,asc"
+    base_url = "https://www.imdb.com/search/title/?groups=oscar_nominee&sort=release_date,asc"
     loop = asyncio.get_event_loop()
     reviews = loop.run_until_complete(traverse_imdb_movies(base_url))
     # reviews = loop.run_until_complete(get_movie_reviews("https://www.imdb.com/title/tt10366460/?ref_=adv_li_tt"))
